0af31eb83ebbafb9f41054344bab1db848822deb
Hossein <hossienbehzadi1381@gmail.com>
2024-08-20 02:15:27 +0330

    run from file admition

--- Backend_UT/main.py
+++ Backend_UT/main.py
@@ -76,3 +76,15 @@ Base.metadata.create_all(engine)
 @app.get('/')
 async def welcome():
     return 'Welcome To Our Project'
+
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(
+        'main:app',
+        host='0.0.0.0',
+        port=8000,
+        reload=True,
+        log_level='debug'
+    )
