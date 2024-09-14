005954c4543b4ab158b642f6572b67f0220c5279
elafatal <vahediielahe@gmail.com>
2024-09-15 02:46:37 +0330

    error component

--- Frontend_UT/src/Components/Relations/Sub-Teacher/SubjectTeachers.jsx
+++ Frontend_UT/src/Components/Relations/Sub-Teacher/SubjectTeachers.jsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 import ReviewCard from '../../SearchBar/search page/Cards/TeacherCard';
 import TCard from './TCard';
 import { Button } from 'bootstrap';
+import Error404 from '../../Error/Error404';
 
 const SubjectTeachers = () => {
     const navigate = useNavigate();
@@ -16,9 +17,7 @@ const SubjectTeachers = () => {
             setSubjectId(location.state.subject_id); 
         }
     }, [location.state]);
-    const handleBackHome = ()=> {
-        navigate('/search', { replace: true });
-      }
+   
     useEffect(() => {
         const handleAllData = async () => {
             console.log("Subject ID to fetch data for:", subject_id);
@@ -65,25 +64,7 @@ const SubjectTeachers = () => {
                         
                     ))
                     
-                    : <main className="farmer-motion h-80 w-full flex flex-col justify-center items-center">
-                    <h1 className="text-9xl font-extrabold text-white tracking-widest">404</h1>
-                    <div className="bg-red-900 px-2 text-sm rounded rotate-12 absolute">
-                        نتیجه‌ای یافت نشد
-                    </div>
-                    <button class="mt-5">
-                      <a
-                        className="relative inline-block text-sm font-medium text-red-900 group active:text-red-500 focus:outline-none focus:ring"
-                      >
-                        <span
-                          className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-red-900 group-hover:translate-y-0 group-hover:translate-x-0"
-                        ></span>
-                
-                        <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
-                          <button onClick={handleBackHome}>بازگشت به صفحه‌ی جستجو</button>
-                        </span>
-                      </a>
-                    </button>
-                </main>
+                    : <Error404/>
 }
             </div>
         </>
